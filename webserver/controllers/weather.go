981be03f357dfb3b9334819ecab304f5cf3c8e09
Yosef Brahmantyo Adi Kristanto <kristantoyo@MNCBank.ID>
2022-10-17 14:08:46 +0700

    Add min and max random value parameterized

--- webserver/controllers/weather.go
+++ webserver/controllers/weather.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"assesment3/webserver/models"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -9,13 +10,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var min = 1
+var max = 100
+
 func GetWeathers(ctx *gin.Context) {
 	r := rand.NewSource(time.Now().UnixNano())
 	ctx.Header("content-type", "application/json")
 	data := models.Status{
-		Water: rand.New(r).Intn(100),
-		Wind:  rand.New(r).Intn(100),
+		Water: rand.New(r).Intn((max - min + 1) + min),
+		Wind:  rand.New(r).Intn((max - min + 1) + min),
 	}
+	log.Printf("Measurement => Water: %v Wind: %v\n", data.Water, data.Wind)
 	ctx.JSON(http.StatusOK, gin.H{
 		"status": data,
 	})

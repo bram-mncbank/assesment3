bc64d05932a764ab07985cfe5a8fd93296fd372d
Yosef Brahmantyo Adi Kristanto <kristantoyo@MNCBank.ID>
2022-10-17 14:29:12 +0700

    Delete unused lines

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"assesment3/webserver"
-	"html/template"
-	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Status struct {
@@ -12,17 +12,6 @@ type Status struct {
 }
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	webserver.Start()
-}
-
-func HomePage(w http.ResponseWriter, r *http.Request) {
-	stat := Status{
-		water: 4,
-		wind:  3,
-	}
-	tpl, _ := template.ParseFiles("./webserver/index.html")
-	tpl.Execute(w, map[string]interface{}{
-		"Payload": stat,
-		"Title":   "Assesment 3",
-	})
 }
